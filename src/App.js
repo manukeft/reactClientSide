014326928e60f8eb7f913688c6fc608479290f9b
manuel arbe <manuarbe12@gmail.com>
2022-11-10 16:41:59 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,130 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, Link, useParams } from "react-router-dom";
+import Header from "./Header";
+import useFetch from "./useFetch";
 
-function App() {
+const Libros = () => {
+  const libros = useFetch("http://localhost:8080/");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {libros.map((libro, i) => (
+        <li key={i}>
+          <Link to={"/libro/" + libro.titulo}>{libro.titulo}</Link>
+        </li>
+      ))}
+    </ul>
   );
-}
+};
 
+const Libro = () => {
+  // Parámetros de la url
+  const { titulo } = useParams();
+  const libros = useFetch("http://localhost:8080/");
+
+  return (
+    <>
+      {libros
+        .filter((libro) => libro.titulo === titulo)
+        .map((libro, i) => (
+          <section key={i}>
+            <h1>{libro.titulo}</h1>
+            <article>
+              <p>ISBN: {libro.isbn}</p>
+              <p>Autor: <Link to={"/autor/" + libro.autor}>{libro.autor}</Link></p>
+              <p>Año: <Link to={"/year/" + libro.year}>{libro.year}</Link></p>
+              <p>Idioma: {libro.idioma}</p>
+              <p>Editorial: <Link to={"/editorial/" + libro.editorial}>{libro.editorial}</Link></p>
+              <p>Precio: {libro.precio}</p>
+            </article>
+          </section>
+        ))}
+      <p>
+        <Link to="/">Regresar a la home</Link>
+      </p>
+    </>
+  );
+};
+
+const Autor = () => {
+  const { autor } = useParams();
+  const libros = useFetch("http://localhost:8080/");
+
+  return (
+    <>
+      <ul>
+        {libros
+          .filter((libro) => libro.autor === autor)
+          .map((libro) => (
+            <li key={libro.autor}>
+              <h2>Libros del autor/a: {libro.autor}</h2>
+              <Link to={"/libro/"+libro.titulo}>{libro.titulo}</Link>
+            </li>
+          ))}
+      </ul>
+      <p>
+        <Link to="/">Regresar a la home</Link>
+      </p>
+    </>
+  );
+};
+
+const Editorial = () => {
+  const { editorial } = useParams();
+  const libros = useFetch("http://localhost:8080/");
+
+  return (
+    <>
+      <ul>
+        {libros
+          .filter((libro) => libro.editorial === editorial)
+          .map((libro) => (
+            <li key={libro.editorial}>
+              <h2>Libros de la editorial: {libro.editorial}</h2>
+              <Link to={"/libro/" + libro.titulo}>{libro.titulo}</Link>
+            </li>
+          ))}
+      </ul>
+      <p>
+        <Link to="/">Regresar a la home</Link>
+      </p>
+    </>
+  );
+};
+
+const Year = () => {
+  const { year } = useParams();
+  const libros = useFetch("http://localhost:8080/");
+
+  return (
+    <>
+      <ul>
+        {libros
+          .filter((libro) => libro.year === year)
+          .map((libro, i) => (
+            <li key={i}>
+              <h2>Libros del año: {libro.year}</h2>
+              <Link to={"/libro/" + libro.titulo}>{libro.titulo}</Link>
+            </li>
+          ))}
+      </ul>
+      <p>
+        <Link to="/">Regresar a la home</Link>
+      </p>
+    </>
+  );
+};
+
+const App = () => (
+  <>
+    <Header />
+    <Routes>
+      <Route exact path="/" element={<Libros />} />
+      <Route path="/libro/:titulo" element={<Libro />} />
+      <Route path="/autor/:autor" element={<Autor />} />
+      <Route path="/editorial/:editorial" element={<Editorial />} />
+      <Route path="/year/:year" element={<Year />} />
+    </Routes>
+  </>
+);
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const Header= () => (
+    <header>          
+        <h1>Los mejores libros del Mundo</h1>
+    </header>
+)
+
+export default Header;
